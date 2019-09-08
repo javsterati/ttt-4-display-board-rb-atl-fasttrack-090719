ccf77d7318bee1ec6a953e7021c74634f014cfa0
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-08 14:11:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board 
-  puts d
+def display_board(board)
+  board = [" "," "," "," "," "," "," "," "," "]
+  puts board
 end
